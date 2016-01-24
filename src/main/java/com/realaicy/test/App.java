997052561a9270a2ec8bf5eb51619246a74c0c7a
realicode <realicode@gmail.com>
2016-01-25 06:09:12 +0800

    ini

---
+++ src/main/java/com/realaicy/test/App.java
@@ -0,0 +1,13 @@
+package com.realaicy.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Mars! 中国" );
+    }
+}
